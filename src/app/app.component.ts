dec734af1b977c23049bc668386044d4a64d7837
calv00 <javicalvosaxo@gmail.com>
2017-06-06 14:21:18 +0200

    Added route to home on app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  { 
   title = 'SAVT Dashboard'; 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  goToDetail(): void {
+        this.router.navigate(['/']);
+    }
 }
