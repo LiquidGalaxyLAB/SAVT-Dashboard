fba28c34bcfa03e8f5db8e59ca2a4563358a7150
calv00 <javicalvosaxo@gmail.com>
2017-06-07 16:02:25 +0200

    Added AnonymousSubscription to refresh values every X seconds.

--- src/app/sensors.component.ts
+++ src/app/sensors.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Sensor } from './sensor';
 import { SensorService } from './sensor.service';
 
+import { Observable } from 'rxjs/Rx';
+import { AnonymousSubscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'sensors',
@@ -13,13 +15,41 @@ import { SensorService } from './sensor.service';
 export class SensorsComponent implements OnInit {
     sensors: Sensor[];
     errorMessage: string;
+    
+    private timerSubscription: AnonymousSubscription;
+    private postsSubscription: AnonymousSubscription;
 
     constructor(private sensorService: SensorService,
         private router: Router) { }
 
     ngOnInit() {
-        this.getSensors();
+        this.refreshData();
      }
+
+    public ngOnDestroy(): void {
+        if (this.postsSubscription) {
+            this.postsSubscription.unsubscribe();
+        }
+        if (this.timerSubscription) {
+            this.timerSubscription.unsubscribe();
+        }
+    }
+
+    private subscribeToData(): void {
+        this.timerSubscription=Observable.timer(5000)  
+    .subscribe(()=>this.refreshData()); 
+    }
+
+    private refreshData(): void {
+        this.postsSubscription = this.sensorService.getSensors()
+    .subscribe(
+        sensors => {
+            this.sensors = sensors;
+            this.subscribeToData();
+        },
+        error => this.errorMessage = <any>error
+        );
+    }
 
     getSensors(): void {
         this.sensorService.getSensors()
