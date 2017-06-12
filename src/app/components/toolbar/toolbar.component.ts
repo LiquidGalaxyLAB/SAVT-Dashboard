e3da07a0ede7340ba2f3b816627895bca8f499b5
calv00 <javicalvosaxo@gmail.com>
2017-06-12 12:38:10 +0200

    Added navigation to home page by toolbar title.

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'toolbar',
@@ -8,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class ToolbarComponent implements OnInit {
     title = 'SAVT Dashboard'; 
-    constructor(
-    ) { }
+    constructor(private router: Router ) { }
 
     ngOnInit() { }
+
+    goToHome(): void {
+        this.router.navigate(['/']);
+    }
 }
