04f37602c11aea0a8e0df423e3e39dfab6796cae
calv00 <javicalvosaxo@gmail.com>
2017-06-22 18:01:40 +0200

    Started again with Sidenav component, has to polish.

--- src/app/modules/toolbar/toolbar.module.ts
+++ src/app/modules/toolbar/toolbar.module.ts
@@ -3,12 +3,14 @@ import { MdToolbarModule } from '@angular/material';
 import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 import { MdButtonModule } from '@angular/material';
 import { MdIconModule } from '@angular/material';
+import {MdSidenavModule} from '@angular/material';
 
 @NgModule({
     imports: [
         MdButtonModule,
         MdIconModule,
-        MdToolbarModule
+        MdToolbarModule,
+        MdSidenavModule
     ],
     exports: [
         ToolbarComponent
